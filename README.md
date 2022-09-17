cf55a13b360afd674310a7167618c743e3b163cc
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-09-17 20:25:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-ylerks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ylerks)
---
+++ script.js
@@ -0,0 +1,11 @@
+function iterate(n) {
+  for (let i = 0; i <= n; i++)
+
+    if (i % 2 == 0) {
+      console.log(i + " is even");
+    }
+    else {
+      console.log(i + " is odd");
+    }
+}
+iterate(15)
